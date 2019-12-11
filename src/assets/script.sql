438a56a970140f0aeb71eca46285804213da52e6
Alex Caiza <alex.raul.caiza.morillo@gmail.com>
2019-12-11 06:52:58 -0500

    cambios commit

---
+++ src/assets/script.sql
@@ -0,0 +1,4 @@
+create table policies( id int not null auto_increment, number varchar(20), amount float, primary key(id));
+
+
+create table games( id int not null auto_increment, name varchar(20),price float, primary key(id));
